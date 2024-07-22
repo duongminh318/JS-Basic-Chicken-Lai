6be80b12875ddb53aeda4177e5f271d5c7a73fcf
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-22 16:49:51 +0700

    add debug choise

--- 5.1-giai-baitap01/main.js
+++ 5.1-giai-baitap01/main.js
@@ -2,7 +2,7 @@
 // let userName;
 
 // hiển thị hộp nhập liệu
-let userName = prompt("Mời cụ nhập tên: ");
+//let userName = prompt("Mời cụ nhập tên: ");
 console.log(userName);
 console.log("Tên của bạn là: " + userName);
 console.log("Chào mừng " + userName + " đến với trang tuhoc.cc");
